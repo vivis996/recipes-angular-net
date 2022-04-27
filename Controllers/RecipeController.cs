508b34b4cc9e892eebe122d491a0974a804cdd42
Daniel Viveros <dviveros90@gmail.com>
2022-04-26 21:24:49 -0400

    Recipe endpoins were added

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -32,4 +32,54 @@ public class RecipeController : ControllerBase
                     .Include(r => r.Ingredients).ToListAsync();
     return Ok(recipes);
   }
+
+  [HttpGet]
+  [Route("{id}")]
+  public async Task<ActionResult<Recipe>> Get(int id)
+  {
+    var recipe = await this._dataContext.Recipes
+                    .Include(r => r.Ingredients)
+                    .FirstOrDefaultAsync(r => r.Id == id);
+    return Ok(recipe);
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<Recipe>> Post([FromBody]Recipe newRecipe)
+  {
+    this._dataContext.Recipes.Add(newRecipe);
+    await this._dataContext.SaveChangesAsync();
+    return Ok(newRecipe);
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<Recipe>> Put([FromBody]Recipe updatedRecipe)
+  {
+    var recipe = await this._dataContext.Recipes.FindAsync(updatedRecipe.Id);
+    if (recipe == null)
+    {
+      throw new NullReferenceException("Recipe not found.");
+    }
+    recipe.Name = updatedRecipe.Name;
+    recipe.Difficulty = updatedRecipe.Difficulty;
+    recipe.PeopleCount = updatedRecipe.PeopleCount;
+    recipe.Time = updatedRecipe.Time;
+
+    await this._dataContext.SaveChangesAsync();
+    return Ok(updatedRecipe);
+  }
+
+  [HttpDelete("{id}")]
+  public async Task<ActionResult<IEnumerable<Recipe>>> Delete(int id)
+  {
+    var recipe = await this._dataContext.Recipes.FindAsync(id);
+    if (recipe == null)
+    {
+      throw new NullReferenceException("Recipe not found.");
+    }
+
+    this._dataContext.Recipes.Remove(recipe);
+    await this._dataContext.SaveChangesAsync();
+
+    return await this.Get();
+  }
 }
