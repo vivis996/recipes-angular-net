17139521a4bfe9fffe2f7fe23eb067d74f2aaffa
Daniel Viveros <dviveros90@gmail.com>
2022-04-27 00:23:44 -0400

    Validations and model response were added

--- Services/RecipeService/IRecipeService.cs
+++ Services/RecipeService/IRecipeService.cs
@@ -4,9 +4,9 @@ namespace Recipes.Services.RecipeService;
 
 public interface IRecipeService
 {
-  Task<List<Recipe>> GetAll();
-  Task<Recipe> GetById(int id);
-  Task<List<Recipe>> AddNewObject(Recipe newObject);
-  Task<Recipe> UpdateObject(Recipe updateObject);
-  Task<List<Recipe>> DeleteObject(int id);
+  Task<ServiceResponse<List<Recipe>>> GetAll();
+  Task<ServiceResponse<Recipe>> GetById(int id);
+  Task<ServiceResponse<List<Recipe>>> AddNewObject(Recipe newObject);
+  Task<ServiceResponse<Recipe>> UpdateObject(Recipe updateObject);
+  Task<ServiceResponse<List<Recipe>>> DeleteObject(int id);
 }
--- Services/RecipeService/RecipeService.cs
+++ Services/RecipeService/RecipeService.cs
@@ -8,56 +8,114 @@ public class RecipeService : BaseService, IRecipeService
   {
   }
 
-  public async Task<List<Recipe>> AddNewObject(Recipe newObject)
+  public async Task<ServiceResponse<List<Recipe>>> AddNewObject(Recipe newObject)
   {
-    this._dataContext.Recipes.Add(newObject);
-    await this._dataContext.SaveChangesAsync();
-    return new List<Recipe> { newObject };
+    var serviceResponse = new ServiceResponse<List<Recipe>>();
+    try
+    {
+      this._dataContext.Recipes.Add(newObject);
+      await this._dataContext.SaveChangesAsync();
+      serviceResponse.Data = new List<Recipe> { newObject };
+    }
+    catch (System.Exception ex)
+    {
+      serviceResponse.Success = false;
+      serviceResponse.Message = ex.Message;
+    }
+    return serviceResponse;
   }
 
-  public async Task<List<Recipe>> DeleteObject(int id)
+  public async Task<ServiceResponse<List<Recipe>>> DeleteObject(int id)
   {
-    var recipe = await this._dataContext.Recipes.FindAsync(id);
-    if (recipe == null)
+    var serviceResponse = new ServiceResponse<List<Recipe>>();
+    try
     {
-      throw new NullReferenceException("Recipe not found.");
+      var recipe = await this._dataContext.Recipes.FindAsync(id);
+      if (recipe == null)
+      {
+        throw new NullReferenceException("Recipe not found.");
+      }
+
+      this._dataContext.Recipes.Remove(recipe);
+      await this._dataContext.SaveChangesAsync();
+      serviceResponse.Data = await this._dataContext.Recipes
+                                .Include(r => r.Ingredients).ToListAsync();
+    }
+    catch (System.Exception ex)
+    {
+      serviceResponse.Success = false;
+      serviceResponse.Message = ex.Message;
     }
 
-    this._dataContext.Recipes.Remove(recipe);
-    await this._dataContext.SaveChangesAsync();
-
-    return await GetAll();
+    return serviceResponse;
   }
 
-  public async Task<List<Recipe>> GetAll()
+  public async Task<ServiceResponse<List<Recipe>>> GetAll()
   {
-    var recipes = await this._dataContext.Recipes
-                    .Include(r => r.Ingredients).ToListAsync();
-    return recipes;
-  }
-
-  public async Task<Recipe> GetById(int id)
-  {
-    var recipe = await this._dataContext.Recipes
-                    .Include(r => r.Ingredients)
-                    .FirstOrDefaultAsync(r => r.Id == id);
-    return recipe;
-  }
-
-  public async Task<Recipe> UpdateObject(Recipe updateObject)
-  {
-    var recipe = await this._dataContext.Recipes.FindAsync(updateObject.Id);
-    if (recipe == null)
+    var serviceResponse = new ServiceResponse<List<Recipe>>();
+    try
     {
-      throw new NullReferenceException("Recipe not found.");
+      serviceResponse.Data = await this._dataContext.Recipes
+                                .Include(r => r.Ingredients).ToListAsync();
     }
-    
-    recipe.Name = updateObject.Name;
-    recipe.Difficulty = updateObject.Difficulty;
-    recipe.PeopleCount = updateObject.PeopleCount;
-    recipe.Time = updateObject.Time;
+    catch (System.Exception ex)
+    {
+      serviceResponse.Success = false;
+      serviceResponse.Message = ex.Message;
+    }
 
-    await this._dataContext.SaveChangesAsync();
-    return recipe;
+    return serviceResponse;
+  }
+
+  public async Task<ServiceResponse<Recipe>> GetById(int id)
+  {
+    var serviceResponse = new ServiceResponse<Recipe>();
+    try
+    {
+      var recipe = await this._dataContext.Recipes
+                      .Include(r => r.Ingredients)
+                      .FirstOrDefaultAsync(r => r.Id == id);
+      if (recipe == null)
+      {
+        throw new NullReferenceException("Recipe not found.");
+      }
+      
+      serviceResponse.Data = recipe;
+    }
+    catch (System.Exception ex)
+    {
+      serviceResponse.Success = false;
+      serviceResponse.Message = ex.Message;
+    }
+
+    return serviceResponse;
+  }
+
+  public async Task<ServiceResponse<Recipe>> UpdateObject(Recipe updateObject)
+  {
+    var serviceResponse = new ServiceResponse<Recipe>();
+    try
+    {
+      var recipe = await this._dataContext.Recipes.FindAsync(updateObject.Id);
+      if (recipe == null)
+      {
+        throw new NullReferenceException("Recipe not found.");
+      }
+      
+      recipe.Name = updateObject.Name;
+      recipe.Difficulty = updateObject.Difficulty;
+      recipe.PeopleCount = updateObject.PeopleCount;
+      recipe.Time = updateObject.Time;
+
+      await this._dataContext.SaveChangesAsync();
+      
+      serviceResponse.Data = recipe;
+    }
+    catch (System.Exception ex)
+    {
+      serviceResponse.Success = false;
+      serviceResponse.Message = ex.Message;
+    }
+    return serviceResponse;
   }
 }
